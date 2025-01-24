82bc228e32c6e3829cb862cdf0dc705577d31c62
sssh <shihar1994@gmail.com>
2025-01-24 10:27:10 +0530

    Java Log File Working Backbone

---
+++ src/main/java/org/example/Log.java
@@ -0,0 +1,84 @@
+package org.example;
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.FileWriter;   // Import the FileWriter class
+
+public class Log {
+
+    private String m_fileName;
+    private static Log m_instance = null;
+    private FileWriter m_fileWriter = null;
+
+    private Log()
+    {
+        System.out.println("A Singleton Log Object Created");
+    }
+
+    public static synchronized Log getInstance()
+    {
+        if (m_instance == null)
+        {
+            m_instance = new Log();
+        }
+
+
+        return m_instance;
+    }
+
+    public void init(String fileName)
+    {
+        m_fileName = fileName;
+
+        try {
+            File myObj = new File(m_fileName);
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        try
+        {
+            m_fileWriter = new FileWriter(m_fileName);
+        }
+        catch (IOException e)
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+
+
+
+    }
+
+    public void writeLog(String content)
+    {
+        try {
+            m_fileWriter.write(content);
+
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+    }
+
+    private void flush() throws IOException {
+        m_fileWriter.flush();
+    }
+
+    public void close() throws IOException {
+        flush();
+        m_fileWriter.close();
+    }
+
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+
+        String s = "Geels";
+        String res = String.format("Welcome to %s! %d %s", "geeks" , 4 , "hello");
+        Log logInstance = Log.getInstance();
+        logInstance.init("Hello1.txt");
+        logInstance.writeLog("hello world" + "\n");
+        logInstance.writeLog(res + "\n");
+        logInstance.writeLog("hello world\n");
         System.out.println("Hello world!");
+        System.out.println(s);
+        logInstance.close();
+        //logInstance.writeLog(res);
     }
 }
