0439f03c4a3eff293cb8666dd8148394f6815c0a
sssh <shihar1994@gmail.com>
2025-01-24 11:14:05 +0530

    Thread new class created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,5 @@
 package org.example;
-
 import java.io.IOException;
-
-class ThreadImpl extends Thread
-{
-    // Method to start Thread
-    private Log m_logInstance = null;
-    ThreadImpl(Log logInstance)
-    {
-        m_logInstance = logInstance;
-    }
-    @Override
-    public void run()
-    {
-        String str = "Thread Class Implementation Thread"
-                + " is Running Successfully";
-        System.out.println(str);
-        String res = String.format("Welcome to %s! %d %s", "geeks" , 4 , "hello");
-
-        m_logInstance.writeLog("hello world" + "\n");
-        m_logInstance.writeLog(res + "\n");
-    }
-}
 
 public class Main {
     public static void main(String[] args) throws IOException {
---
+++ src/main/java/org/example/ThreadImpl.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class ThreadImpl extends Thread{
+    // Method to start Thread
+    private Log m_logInstance = null;
+    ThreadImpl(Log logInstance)
+    {
+        m_logInstance = logInstance;
+    }
+    @Override
+    public void run()
+    {
+        String str = "Thread Class Implementation Thread"
+                + " is Running Successfully";
+        System.out.println(str);
+        String res = String.format("Welcome to %s! %d %s", "geeks" , 4 , "hello");
+        m_logInstance.writeLog("hello world" + "\n");
+        m_logInstance.writeLog(res + "\n");
+    }
+
+}
